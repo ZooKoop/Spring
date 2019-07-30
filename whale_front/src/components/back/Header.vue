fb4f51785cb3bfc4718b09b98de4f61389011f2c
ZooKoop <769442926@qq.com>
2019-07-30 17:31:42 +0800

    前端分离

---
+++ whale_front/src/components/back/Header.vue
@@ -0,0 +1,33 @@
+<template>
+	<!-- 	<div class="whale_bg">
+		<img class="whale_img whale_middle" alt="" src="img/blue.png">
+		<div class="whale_pop">
+			<div class="whale_pop_gao"></div>
+		</div>
+	</div> -->
+	<div class="index_conent whale_middle">
+		<p class="whale_p"><span>W</span>hale</p>
+		<div></div>
+		<ul class="whale_U">
+			<li class="whale_U_li">A</li>
+			<li class="whale_U_li">BBB</li>
+			<li class="whale_U_li">C</li>
+			<li class="whale_U_li">D</li>
+			<li class="whale_U_li">E</li>
+		</ul>
+	</div>
+
+</template>
+
+<script>
+	export default {
+		name: 'Header',
+		data() {
+			return {
+				msg1: '111111111111111111111111'
+			}
+		}
+	}
+</script>
+<style>
+</style>
